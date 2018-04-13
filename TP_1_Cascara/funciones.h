62af11b76a555e03be2c5e0318ace17c8f662028
xaxwtf <36156872+xaxwtf@users.noreply.github.com>
2018-04-12 21:29:30 -0300

    Add files via upload
    12/04/18

---
+++ TP_1_Cascara/funciones.h
@@ -0,0 +1,46 @@
+/** \brief solicita un numero del Usuario(en este caso un flotante)y muestra el mensaje indicado por el Usuario
+ *
+ * \param Mensaje es el mensaje a ser mostrado
+
+ * \return El Numero Ingresado por el Usuario
+ *
+ */
+
+float pedirflotante(char mensaje[]);
+/** \brief Realiza la suma Entre 2 numeros
+ *
+ * \param es el 1er numero indicado por el Usuario
+ * \param es el 2do numero indicado por el Usuario
+ * \return el resultado de la suma entre el 1er numero y el 2do
+ *
+ */
+
+float sumar(float,float);
+/** \brief Realiza una resta entre 2 numeros
+ *
+ * \param es el 1er numero indicado por el Usuario
+ * \param es el 2do numero indicado por el Usuario
+ * \return El resultado de la Resta entre los 2 numeros
+ *
+ */
+
+float restar(float,float);
+/** \brief Realiza una divicion entre los numeros ingresados en el orden que se ingresa, si el 2do numero es "0"devuelve 0 y envia un mensaje
+ *
+ * \param es el 1er numero indicado por el Usuario
+ * \param es el 2do numero indicado por el Usuario
+ * \return El resultado de la divicion entre los 2 numeros
+ *
+ */
+
+float dividir(float,float);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+
+float multiplicar(float,float);
